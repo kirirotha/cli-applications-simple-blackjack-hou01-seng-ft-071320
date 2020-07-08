9026b881b2a9c69409e838993cfdc33b1cead266
Kiri Um <kirirotha@gmail.com>
2020-07-08 15:27:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,7 +50,7 @@ def hit?(card_total)
   if user_in == "h"
     new_card = deal_card
     card_total = card_total + new_card
-  elsif user_in != "s"
+  elsif user_in == "s"
     return
   else
     invalid_command
